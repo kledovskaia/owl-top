22dcfe6caf2130c9742b1e7e36742ba3eab5c283
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-26 12:26:45 +0300

    added: Footer component

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import cn from 'classnames';
+import styles from './Footer.module.scss';
 
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
 
-const Footer: FC<Props> = (props) => {
-  return <footer {...props}>Footer</footer>;
+const Footer: FC<Props> = ({ className, ...props }) => {
+  const year = new Date().getFullYear();
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <p className={styles.footer__copy}>
+        OwlTop © 2020 - {year} Все права защищены
+      </p>
+      <a className={styles.footer__link} href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a className={styles.footer__link} href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
