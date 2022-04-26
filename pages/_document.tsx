67c960e86024426c710e7d454d7131cd300d6cb2
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-26 15:14:11 +0300

    fixed: Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,28 @@
-import { Html, Main, NextScript, Head } from 'next/document';
-import { FC } from 'react';
+import Document, { Html, Main, NextScript, Head } from 'next/document';
 
-const Document: FC = () => (
-  <Html>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="crossOrigin"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="crossOrigin"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-export default Document;
+export default MyDocument;
