731e2e6b324e36b79386a6127f3bd3d892dda3d2
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-28 05:51:56 +0300

    added: basic sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -6,9 +6,29 @@ type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 const Sidebar: FC<Props> = (props) => {
   const { menu } = useMenu();
 
-  console.log(menu);
-
-  return <div {...props}></div>;
+  return (
+    <div {...props}>
+      <ul>
+        {menu.map((firstLevel) => (
+          <li key={firstLevel.firstCategory}>
+            <h2>{firstLevel.firstCategoryName}</h2>
+            <ul>
+              {firstLevel.menu.map((secondLevel) => (
+                <li key={secondLevel._id.secondCategory}>
+                  <h3>{secondLevel._id.secondCategory}</h3>
+                  <ul>
+                    {secondLevel.pages.map((thirdLevel) => (
+                      <li key={thirdLevel._id}>{thirdLevel.title}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
